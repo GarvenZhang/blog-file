55eef77d9afdd5c896c57abfbf4b6c74fdfa83a5
931923959 <931923959@qq.com>
2018-08-05 16:11:36 +0800

    存些重要的包或者项目

--- server/controller/dictionary.js
+++
@@ -1,8 +0,0 @@
-const dictionary = require('../db/dictionary')
-
-exports.get = function (ctx) {
-  const cb = ctx.query.cb
-  ctx.set('Content-Type', 'application/javascript')
-  ctx.body = `${cb}(${JSON.stringify(dictionary)})`
-}
-
